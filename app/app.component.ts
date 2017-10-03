905d42077548c997c722eea3bb3891f058f3f39f
bhagath <bhagath@getcustomexposure.com>
2017-10-03 19:01:51 +0530

    creating app component

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'app',
+	template: '<h1>Navigation</h1>'
+})
+
+export class AppComponent{
+	title: 'Freelance Bootcamp Dashboard';
+
+}
