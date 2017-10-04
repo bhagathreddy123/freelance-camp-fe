511c523a67c41e8a4433dc0b2a712f7ec09d6288
bhagath <bhagath@getcustomexposure.com>
2017-10-04 06:40:04 +0530

    accessing component data from an angular:

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -7,6 +7,8 @@ import { Document } from './document';
 	templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent{
+pageTitle: string = "Document DashBoard"
+
 	documents: Document[] = 
 	[
 			{
